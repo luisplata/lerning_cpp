1d00947a7ed882eccaf0d0589b348d9b09e05841
Luis Plata <www.luisplata@gmail.com>
2024-09-02 20:26:43 -0500

    Dasher Fisrt Day

---
+++ Dasher/dasher.cpp
@@ -0,0 +1,96 @@
+#include "raylib.h"
+int main()
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "Dasher");
+
+    // rules of game
+    // int maxJumpHeight = 700;
+    const int gravity{1000};
+    bool isInAir = false;
+    int jumpVelocity = -600;
+
+    //Animation variables
+    int frame = 0;
+    float updateTime {1.0f / 12.0f}; // 12 frames per second
+    float runningTime{0.0f};
+
+    // initial position of the rectangle
+    // graphics
+    // Player
+    Texture2D player = LoadTexture("textures/scarfy.png");
+    Rectangle playerRec;
+    playerRec.width = player.width / 6;
+    playerRec.height = player.height;
+    playerRec.x = frame * playerRec.width;
+    playerRec.y = 0;
+    const float lineOfGround = screenHeight * 0.8 - playerRec.height;
+    Vector2 playerPosition = {screenWidth / 2 - playerRec.width / 2, lineOfGround};
+    int velocityY = 0;
+
+    // Enemy
+    Texture2D enemy = LoadTexture("textures/12_nebula_spritesheet.png");
+    Rectangle enemyRec;
+    enemyRec.width = enemy.width / 8;
+    enemyRec.height = enemy.height / 8;
+    enemyRec.x = 0;
+    enemyRec.y = 0;
+
+
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        // Apply deltaTime
+        float dt{GetFrameTime()};
+
+        // check if player is on the ground
+        if (playerPosition.y >= screenHeight * 0.8 - playerRec.height)
+        {
+            velocityY = 0;
+            isInAir = false;
+        }
+        else
+        {
+            velocityY += gravity * dt;
+            isInAir = true;
+        }
+
+        // Inputs
+        if (IsKeyPressed(KEY_SPACE) && !isInAir)
+        {
+            velocityY += jumpVelocity;
+        }
+
+        playerPosition.y += velocityY * dt;
+
+        // Animation
+        runningTime += dt;
+        if (runningTime >= updateTime)
+        {
+            runningTime = 0.0f;
+            frame++;
+            if (frame > 5)
+            {
+                frame = 0;
+            }
+        }
+        playerRec.x = frame * playerRec.width;
+
+        // Draw objects
+
+        // player
+        DrawTextureRec(player, playerRec, playerPosition, WHITE);
+
+        // End Drawing
+        EndDrawing();
+    }
+    UnloadTexture(player);
+    UnloadTexture(enemy);
+    CloseWindow();
+    return 0;
+}
